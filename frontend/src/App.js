01505fba4e74f660b57e10b66cfe69826b752eb1
JulieMontoux <julie.montoux@epsi.fr>
2025-04-05 22:19:52 +0200

    fix : import routes front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Routes, Route  } from "react-router-dom";
 import AppNavbar from "./components/Navbar";
 import LoginForm from "./components/LoginForm";
 import HomePage from "./pages/HomePage";
