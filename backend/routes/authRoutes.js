9d6a5f98eacc031d2375641a7bd3ce5f9c67f45c
JulieMontoux <julie.montoux@epsi.fr>
2025-04-06 00:22:46 +0200

    supp doublon router exports

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,18 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('./../controllers/authController');
+const authController = require("./../controllers/authController");
 
 const getAppMode = require("./../middleware/getAppMode");
 const loginLimiter = require("./../middleware/rateLimiter");
 
-router.post("/login", (req, res, next) => {
+router.post(
+  "/login",
+  (req, res, next) => {
     const isVulnerable = getAppMode(req);
     if (!isVulnerable) {
       return loginLimiter(req, res, next); // ⛔ sécurise seulement en mode sécurisé
     }
     next();
-  }, authController.login);
-  
-  module.exports = router;
+  },
+  authController.login
+);
 
 module.exports = router;
